943077fee0e0fde83411700cc0c6d07ed3e4244f
lemontreeran <ran.xing@gmail.com>
2025-03-24 01:30:52 +1100

    Bot test

--- qallmagent-slackbot/api/index.js
+++ qallmagent-slackbot/api/index.js
@@ -16,4 +16,9 @@ app.command('/ping', async ({ ack, say }) => {
     await say('Pong!');
 });
 
+expressReceiver.app.use((req, res, next) => {
+    console.log("🔥 Received unknown route", req.method, req.url);
+    next();
+});
+
 module.exports = expressReceiver.app;
